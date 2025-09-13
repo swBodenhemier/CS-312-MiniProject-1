bd8faf77aabede0db910622fea8a8d4dddf097b1
Samuel Bodenhemier <swb79@nau.edu>
2025-09-13 15:20:24 -0700

    Implemented post editing

--- index.js
+++ index.js
@@ -15,20 +15,40 @@ app.use(morgan("dev"));
 let posts = [];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { posts: posts, editing: false });
+  res.render("index.ejs", { posts: posts.slice().reverse(), status: "none" });
 });
 
 app.post("/newPost", (req, res) => {
-  console.log(req.body);
-  res.render("index.ejs", { posts: posts, editing: true });
+  res.render("index.ejs", { posts: posts.slice().reverse(), status: "create" });
 });
 
 app.post("/addPost", (req, res) => {
-  newPost(req.body.user, req.body.title, req.body.body);
+  if (req.body.submit !== "Cancel") {
+    newPost(req.body.user, req.body.title, req.body.body);
+  }
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  const postID = Object.keys(req.body)[0];
+  res.render("index.ejs", {
+    posts: posts.slice().reverse(),
+    status: "edit",
+    postID: postID,
+  });
+});
+
+app.post("/changePost", (req, res) => {
+  const postID = Object.keys(req.body).filter((key) => !isNaN(Number(key)))[0];
+  const post = posts.filter((post) => post.id === Number(postID))[0];
+  if (post) {
+    post.date = `edited: ${newDateString(post.id)}`;
+    post.userName = req.body.user;
+    post.title = req.body.title;
+    post.body = req.body.body;
+  }
+  res.redirect("/");
+});
 
 app.post("/delete", (req, res) => {
   const postID = Object.keys(req.body)[0];
@@ -42,13 +62,17 @@ app.listen(port, () => {
 
 function newPost(user, title, body) {
   const postID = Date.now();
-  const date = new Date(postID);
   const postObj = {
     id: Number(postID),
     userName: user && user.length > 0 ? user : "no-user",
-    date: date.toDateString(),
-    title: title && title.length > 0 ? title : "no-title",
-    body: body && body.length > 0 ? body : "no-body",
+    date: newDateString(postID),
+    title: title && title.length > 0 ? title : "no title",
+    body: body && body.length > 0 ? body : "no content",
   };
   posts.push(postObj);
+}
+
+function newDateString(date) {
+  const dateObj = new Date(date);
+  return `${dateObj.toLocaleDateString()} at ${dateObj.toLocaleTimeString()}`;
 }
