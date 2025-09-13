0f8947ffa8f93acbecf2a56edae1b788614e9d98
Samuel Bodenhemier <swb79@nau.edu>
2025-09-13 13:42:13 -0700

    Implemented post deletion

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
-const posts = [];
+let posts = [];
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { posts: posts, editing: false });
@@ -24,20 +24,16 @@ app.post("/newPost", (req, res) => {
 });
 
 app.post("/addPost", (req, res) => {
-  console.log(req.body);
   newPost(req.body.user, req.body.title, req.body.body);
-  res.render("index.ejs", { posts: posts, editing: false });
-  // res.render("file", {key: variable})
-  /* ejs tags:
-  <%= variable %>
-  <% executable js code %>
-  <%- HTML %>
-  <%% show ejs tag %%>
-  <%# comment %>
-  <%- include(another ejs file) %>
-    ----------
-  if(locals.variable) check for undefined
-  */
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {
+  const postID = Object.keys(req.body)[0];
+  posts = posts.filter((post) => post.id !== Number(postID));
+  res.redirect("/");
 });
 
 app.listen(port, () => {
@@ -48,7 +44,7 @@ function newPost(user, title, body) {
   const postID = Date.now();
   const date = new Date(postID);
   const postObj = {
-    id: postID,
+    id: Number(postID),
     userName: user && user.length > 0 ? user : "no-user",
     date: date.toDateString(),
     title: title && title.length > 0 ? title : "no-title",
